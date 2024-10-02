a2c3b517c627b1876fe35826efb9cb351ceb4939
Mitiu <dumitrubjr1@gmail.com>
2024-10-01 20:19:51 -0400

    Finished Sentinel.asm

--- DB313HW2/sentinel.asm
+++ DB313HW2/sentinel.asm
@@ -2,7 +2,9 @@
 
 # Data goes here
 .data
-    prompt1: .asciiz "Enter a temperaturo (or -99 to quit): " 
+    prompt1: .asciiz "Enter a temperature: (or -99 to quit): " 
+    finalPrompt: .asciiz "Average temperature: " 
+    newLine: .asciiz "\n"
 
 .text
 
@@ -12,10 +14,10 @@
 
 main:
     # Put sentinel val at $t0,
-    li $t0, -99
+    li.s $f14, -99.0
 
     # init our sum register
-    li $t1, 0
+    li.s $f2, 0.0
 
     #init a count register
     li $t2, 0
@@ -24,25 +26,26 @@ main:
     la $a0, prompt1
     syscall
 
-    li $v0, 5
+    li $v0, 6
     syscall
-    move $t4, $v0
+    mov.s $f12, $f0
 
 while_loop:
     # If the input is -99 break.
-    beq $t4, $t0, exit
+    c.eq.s $f12, $f14
+    bc1t output
 
     # Add the input to the sum and increment our count vars.
-    add $t1, $t1, $t4
+    add.s $f2, $f2, $f12
     addi $t2, $t2, 1
 
     li $v0, 4
     la $a0, prompt1
     syscall
 
-    li $v0, 5
+    li $v0, 6
     syscall
-    move $t4, $v0
+    mov.s $f12, $f0
 
     j while_loop
 
@@ -53,10 +56,29 @@ while_loop:
 
     
 
-exit: 
-    div $t1, $t2
-    
-    li $v0, 10
+output: 
+    beq $t2, $zero, exitFinal
+
+    mtc1 $t2, $f4
+    cvt.s.w $f4, $f4
+
+    div.s $f0, $f2, $f4
+
+    mov.s $f12, $f0
+
+    li $v0, 4
+    la $a0, newLine
     syscall
 
+    li $v0, 4
+    la $a0, finalPrompt
+    syscall
+
+    li $v0, 2
+    syscall
+    
+
+exitFinal:
+    li $v0, 10
+    syscall
 .end main
